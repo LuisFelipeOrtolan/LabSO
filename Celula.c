24044c8cfeb05d7ec3a1af71afb99bcee0dd36b3
Luís Felipe <luisfelipe.ortolan@gmail.com>
2019-10-03 13:58:23 -0300

    Celula.C comentada

--- Celula.c
+++ Celula.c
@@ -5,6 +5,10 @@
 
 int nroProc = 1;
 
+/*Função responsável por reorganizar os números que correspondem a posição
+na fila dos processos em segundo plano ou parados depois que um deles foi removido
+Inputs: -
+Output: uma lista com valores reorganizados*/
 void rearranjaChaves(Celula *lst, int chave){
 	Celula *p = lst;
 	while(p != NULL){
@@ -13,7 +17,12 @@ void rearranjaChaves(Celula *lst, int chave){
 		p = p->prox;
 	}
 }
-
+/*Função responsável por inserir um processo na lista dos processos em
+segundo plano. 
+Inputs: Endereço do primeiro nó, pid do processo a ser
+colocado na lista, um ponteiro de ponteiro de char com os comandos e 
+o número de palavras no comando do processo. 
+Output: uma nova lista com a nova célula inserida*/
 void insere(Celula **lst, pid_t pid, char **listaPalavras, int tam){
 	Celula *nova;
 	nova = malloc(sizeof(Celula));
@@ -39,7 +48,10 @@ void insere(Celula **lst, pid_t pid, char **listaPalavras, int tam){
 		p->prox = nova;
 	}
 }
-
+/*Função responsável por imprimir todos os processos em
+segundo plano. 
+Inputs: Endereço do primeiro nó da lista
+Output: Todos as células da lista impressas no terminal*/
 void imprime(Celula *lst){
 	Celula *p = lst;
 	int i = 0;
@@ -54,14 +66,18 @@ void imprime(Celula *lst){
 		p = p->prox;
 	}
 }
-
+/*Função responsável por buscar um elemento na lista pelo PID
+Input: Primeira Celula da lista onde se deseja buscar
+Output: A célula buscada ou NULL se não foi encontrada */
 Celula *busca(Celula *lst, pid_t pid){
 	Celula *p = lst;
 	while(p != NULL && p->pid != pid)
 		p = p->prox;
 	return p;
 }
-
+/*Função responsável por escolher uma célula através da chave associada ao processo
+Input: Primeira Celula da lista onde se deseja buscar
+Output: A célula escolhida ou NULL se ela não existe */
 Celula *selecao(Celula *lst, int pos){
 	Celula *p = lst;
 	while(p != NULL && p->chave < pos){
@@ -69,7 +85,9 @@ Celula *selecao(Celula *lst, int pos){
 	}
 	return p;
 }
-
+/*Função esponsável por retirar uma célula da lista de processos em segundo plano
+Input: O PID da célula que se deseja remover e a primeira célula da lista
+Output: A lista original sem a célula removida ou a mesma lista caso a célula não exista*/
 Celula * retira(Celula *lst, pid_t pid){
 	Celula *p, *q;
 	p = lst;
